fcfbb82e3d7a676a3393ba94e8ace09847f20029
hilmanman92 <ahilmans92@gmail.com>
2024-02-25 17:20:21 +0700

    adding new query and m/l model

---
+++ HR Analytics Dashboard/Query/Import BI.sql
@@ -0,0 +1,41 @@
+-- salary stats
+with 
+salarystats as (
+    select
+        JobLevel,
+        avg(MonthlyIncome) as Avg_Salary,
+        ceiling(stdev(MonthlyIncome)) as StDev_Salary
+    from 
+        EmployeeAttrition
+    group by 
+        JobLevel
+),
+
+-- outlier
+outlierstats as (
+    select
+        emp.JobLevel,
+        round(Avg_Salary - (2*(StDev_Salary)),2) as LowerBoundSalary,
+        round(Avg_Salary + (2*(StDev_Salary)),2) as UpperBoundSalary,
+        st.Avg_Salary,
+        st.StDev_Salary
+    from
+        EmployeeAttrition emp 
+    join 
+        salarystats st on emp.JobLevel = st.JobLevel
+    group by
+        emp.JobLevel, Avg_Salary, StDev_Salary
+)
+
+-- fairness paid
+select
+	emp.*,
+	case
+		when emp.MonthlyIncome < ou.Avg_Salary and emp.MonthlyIncome < ou.LowerBoundSalary then 'Underpaid'
+		when emp.MonthlyIncome > ou.Avg_Salary and emp.MonthlyIncome > ou.UpperBoundSalary then 'Overpaid'
+		else 'Fairpaid'
+		end as PaidFairness
+	from
+		EmployeeAttrition emp 
+		join 
+		outlierstats ou on emp.JobLevel = ou.JobLevel
